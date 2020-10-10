ad6175478b86ba9d49bbd1bc2705f11b02876e65
clubby789 <jamie@hill-daniel.co.uk>
2020-10-10 21:51:01 +0100

    Implement stack operations

--- laser.py
+++ laser.py
@@ -7,43 +7,74 @@ WEST = [-1, 0]
 
 
 INSTRUCTION = 0
-STRING      = 1
-RAW         = 2
+STRING = 1
+RAW = 2
 
 CONDITIONALS = "⌞⌜⌟⌝"
 MIRRORS = "\\/>v<^" + CONDITIONALS
 UNARY = "()crR!~pPoObnB"
 BINARY = "+-×÷*gl=&|%"
 
+
 class LaserStack:
-    contents = []
+    contents = [[]]
+    addr = 0
 
     def pop(self):
-        if len(self.contents) == 0:
+        if len(self.contents[self.addr]) == 0:
             print("Error: pop from empty stack")
             exit(-1)
-        return self.contents.pop()
+        return self.contents[self.addr].pop()
 
     def push(self, item):
-        self.contents.append(item)
+        self.contents[self.addr].append(item)
 
     def peek(self):
-        if len(self.contents) == 0:
+        if len(self.contents[self.addr]) == 0:
             return 0
-        return self.contents[-1]
+        return self.contents[self.addr][-1]
+
+    def sUp(self):
+        self.addr += 1
+        if self.addr == len(self.contents):
+            # Top of the stack
+            self.contents.append(list())
+
+    def sDown(self):
+        self.addr -= 1
+
+    def swUp(self):
+        a = self.contents[self.addr].pop()
+        if self.addr == len(self.contents) - 1:
+            self.contents.append(list())
+        self.contents[self.addr + 1].append(a)
+
+    def swDown(self):
+        a = self.contents[self.addr].pop()
+        self.contents[self.addr - 1].append(a)
+
+    def rUp(self):
+        a = self.contents[self.addr].pop()
+        self.contents[self.addr].insert(0, a)
+
+    def rDn(self):
+        a = self.contents[self.addr].pop()
+        self.contents[self.addr - 1].append(a)
 
     def __repr__(self):
-        return repr(self.contents[::-1])
+        return repr(self.contents[self.addr][::-1])
 
     def __len__(self):
-        return len(self.contents)
+        return len(self.contents[self.addr])
+
 
 class LaserMachine:
     direction = [1, 0]
     memory = LaserStack()
     parse_mode = INSTRUCTION
     current_string = ""
-    def __init__(self, prog, verbose=False):
+
+    def __init__(self, prog, verbose=False, init=[]):
         self.verbose = verbose
         lines = prog.split('\n')
         self.prog = []
@@ -52,14 +83,19 @@ class LaserMachine:
         for line in lines:
             self.prog.append(list(line.ljust(self.width)))
         self.pc = [0, self.height - 1]
+        for var in init[::-1]:
+            self.memory.push(var)
         if verbose:
             for line in self.prog:
                 print(''.join(line))
             self.debug()
 
     def debug(self):
-            print(f"PC: {self.pc} - DIRECTION: {self.direction} - STACK: {self.memory} - MODE: {self.parse_mode}")
-
+        print(f"PC: {self.pc} -"
+              f"DIRECTION: {self.direction} -"
+              f"STACK: {self.memory} -"
+              f"MODE: {self.parse_mode}"
+              )
 
     def do_step(self):
         i = self.fetch_item()
@@ -70,7 +106,7 @@ class LaserMachine:
         elif i in MIRRORS:
             self.switch_direction(i)
         elif self.parse_mode == INSTRUCTION:
-            self.process_instruction( i)
+            self.process_instruction(i)
         elif self.parse_mode == STRING:
             self.process_string(i)
         xMov, yMov = self.direction[0], self.direction[1]
@@ -92,46 +128,46 @@ class LaserMachine:
 
         # UNARY OPERATIONS #
 
-        elif i == "(": # DEC
+        elif i == "(":  # DEC
             val = self.memory.pop()
             val -= 1
             self.memory.push(val)
-        elif i == ")": # INC
+        elif i == ")":  # INC
             val = self.memory.pop()
             val += 1
             self.memory.push(val)
-        elif i == "c": # CRD
+        elif i == "c":  # CRD
             val = len(self.memory)
             self.memory.push(val)
-        elif i == "r" or i == "R": # RPL
+        elif i == "r" or i == "R":  # RPL
             # Should function the same for replicating a stack
             # or a value
             val = self.memory.peek()
             self.memory.push(val)
-        elif i == "!": # NBF
+        elif i == "!":  # NBF
             pass
-        elif i == "~": # NBW
+        elif i == "~":  # NBW
             val = self.memory.pop()
             val = ~val
             self.memory.push(val)
-        elif i == "p" or i == "P": # POP
+        elif i == "p" or i == "P":  # POP
             val = self.memory.pop()
-        elif i == "o": # POPO
+        elif i == "o":  # POPO
             val = self.memory.pop()
             print(val)
-        elif i == "O": # POPS
+        elif i == "O":  # POPS
             while len(self.memory) > 0:
                 print(self.memory.pop(), end=' ')
             print('')
-        elif i == "b": # CS
+        elif i == "b":  # CS
             val = self.memory.pop()
             val = chr(val)
             self.memory.push(val)
-        elif i == "n": # CN
+        elif i == "n":  # CN
             val = self.memory.pop()
             for c in val[::-1]:
                 self.memory.push(ord(c))
-        elif i == "B": # CSS
+        elif i == "B":  # CSS
             nums = []
             while type(self.memory.peek()) == int:
                 nums.append(self.memory.pop())
@@ -140,57 +176,72 @@ class LaserMachine:
 
         # BINARY OPERATIONS #
 
-        elif i == "+": # ADD
+        elif i == "+":  # ADD
             val = self.memory.pop() + self.memory.pop()
             self.memory.push(val)
-        elif i == "-": # SUB
+        elif i == "-":  # SUB
             val = self.memory.pop() - self.memory.pop()
             self.memory.push(val)
-        elif i == "×": # MUL
+        elif i == "×":  # MUL
             val = self.memory.pop() * self.memory.pop()
             self.memory.push(val)
-        elif i == "÷": # DIV
+        elif i == "÷":  # DIV
             a = self.memory.pop()
             b = self.memory.pop()
             val = b / a
             self.memory.push(val)
-        elif i == "*": # EXP
+        elif i == "*":  # EXP
             a = self.memory.pop()
             b = self.memory.pop()
             val = b ** a
             self.memory.push(val)
-        elif i == "g": # GRT
+        elif i == "g":  # GRT
             a = self.memory.pop()
             b = self.memory.pop()
             val = int(b > a)
             self.memory.push(val)
-        elif i == "l": # LSS
+        elif i == "l":  # LSS
             a = self.memory.pop()
             b = self.memory.pop()
             val = int(b < a)
             self.memory.push(val)
-        elif i == "u": # EQL
+        elif i == "u":  # EQL
             a = self.memory.pop()
             b = self.memory.pop()
             val = int(b == a)
             self.memory.push(val)
-        elif i == "&": # AND
+        elif i == "&":  # AND
             val = int(self.memory.pop & self.memory.pop())
             self.memory.push(val)
-        elif i == "&": # OR
+        elif i == "&":  # OR
             val = int(self.memory.pop | self.memory.pop())
             self.memory.push(val)
-        elif i == "&": # MOD
+        elif i == "&":  # MOD
             a = self.memory.pop()
             b = self.memory.pop()
             val = b % a
             self.memory.push(val)
 
+        # STACK OPERATIONS #
 
+        elif i == "U":  # SUP
+            self.memory.sUp()
+        elif i == "D":  # SDN
+            self.memory.sDown()
+        elif i == "s":  # SWUP
+            self.memory.swUp()
+        elif i == "w":  # SWDN
+            self.memory.swDown()
+
+        elif i == " ":  # NOP
+            pass
         elif i == "#":
             while len(self.memory) > 0:
                 print(self.memory.pop(), end=' ')
             print('')
+            exit()
+        else:
+            print(f"Instruction {i} unknown!")
             exit()
 
     def process_string(self, i):
@@ -203,60 +254,62 @@ class LaserMachine:
 
     def switch_direction(self, mirror):
         if mirror == "\\":
-            if   self.direction == NORTH: self.direction = WEST
-            elif self.direction == WEST : self.direction = NORTH
+            if self.direction == NORTH: self.direction = WEST
+            elif self.direction == WEST: self.direction = NORTH
             elif self.direction == SOUTH: self.direction = EAST
-            elif self.direction == EAST : self.direction = SOUTH
+            elif self.direction == EAST: self.direction = SOUTH
         elif mirror == "/":
             if   self.direction == NORTH: self.direction = EAST
-            elif self.direction == WEST : self.direction = SOUTH
+            elif self.direction == WEST: self.direction = SOUTH
             elif self.direction == SOUTH: self.direction = WEST
-            elif self.direction == EAST: self.direction  = NORTH
+            elif self.direction == EAST: self.direction = NORTH
         elif mirror == ">":
             if   self.direction == NORTH: self.direction = EAST
             elif self.direction == SOUTH: self.direction = EAST
         elif mirror == "v":
-            if self.direction == WEST : self.direction = SOUTH
-            elif self.direction == EAST : self.direction = SOUTH
+            if self.direction == WEST: self.direction = SOUTH
+            elif self.direction == EAST: self.direction = SOUTH
         elif mirror == "<":
             if   self.direction == NORTH: self.direction = WEST
             elif self.direction == SOUTH: self.direction = WEST
         elif mirror == "^":
-            if self.direction == WEST : self.direction = NORTH
-            elif self.direction == EAST: self.direction  = NORTH
+            if self.direction == WEST: self.direction = NORTH
+            elif self.direction == EAST: self.direction = NORTH
         elif mirror in CONDITIONALS:
             if self.memory.peek() == 0:
                 if mirror == "⌞":
                     if   self.direction == NORTH: self.direction = EAST
-                    elif self.direction == WEST : self.direction = NORTH
+                    elif self.direction == WEST: self.direction = NORTH
                     elif self.direction == SOUTH: self.direction = EAST
-                    elif self.direction == EAST : self.direction = NORTH
+                    elif self.direction == EAST: self.direction = NORTH
                 elif mirror == "⌜":
                     if   self.direction == NORTH: self.direction = EAST
-                    elif self.direction == WEST : self.direction = SOUTH
+                    elif self.direction == WEST: self.direction = SOUTH
                     elif self.direction == SOUTH: self.direction = EAST
-                    elif self.direction == EAST : self.direction = SOUTH
+                    elif self.direction == EAST: self.direction = SOUTH
                 elif mirror == "⌟":
                     if   self.direction == NORTH: self.direction = WEST
-                    elif self.direction == WEST : self.direction = NORTH
+                    elif self.direction == WEST: self.direction = NORTH
                     elif self.direction == SOUTH: self.direction = WEST
-                    elif self.direction == EAST : self.direction = NORTH
+                    elif self.direction == EAST: self.direction = NORTH
                 elif mirror == "⌝":
                     if   self.direction == NORTH: self.direction = WEST
-                    elif self.direction == WEST : self.direction = SOUTH
+                    elif self.direction == WEST: self.direction = SOUTH
                     elif self.direction == SOUTH: self.direction = WEST
-                    elif self.direction == EAST : self.direction = SOUTH
+                    elif self.direction == EAST: self.direction = SOUTH
 
 
 parser = argparse.ArgumentParser(description='Run a LaserLang program.')
 parser.add_argument("-v", "--verbose", action="store_true",
                     default=False, help="verbose output")
 parser.add_argument("program", help="Laser program to run")
+parser.add_argument("input", help="Items to initialize stack with",
+                    nargs='*')
 args = parser.parse_args()
 
 with open(args.program, 'r') as f:
     prog = f.read()
 
-LM = LaserMachine(prog, verbose=args.verbose)
+LM = LaserMachine(prog, verbose=args.verbose, init=args.input)
 while LM.do_step():
     pass
