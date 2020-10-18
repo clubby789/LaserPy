839b773381e8868479bd230e280f101ccbcdd2bf
clubby789 <jamie@hill-daniel.co.uk>
2020-10-18 16:34:39 +0100

    Test raw string and verbose mode

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,13 +4,20 @@ import subprocess
 
 def prog_test(program):
     """Test a single program"""
-    if program['args'] is None:
-        cmd = f"python3 laser.py {program['path']}"
-    else:
-        cmd = f"python3 laser.py {program['path']} {program['args']}"
-    print(cmd)
+    stdout = run_prog(program['path'], program['args'])
+    print(list(stdout))
+    assert stdout == program["expected"]
+
+
+def run_prog(path, args, verbose=False):
+    """Run a program and return the output"""
+    cmd = "python3 laser.py "
+    if verbose:
+        cmd += "-v "
+    cmd += f"{path} "
+    if args:
+        cmd += args
     stdout = subprocess.run(cmd.split(),
                             stdout=subprocess.PIPE,
                             check=True).stdout.decode()
-    print(list(stdout))
-    assert stdout == program["expected"]
+    return stdout
--- tests/test_programs.py
+++ tests/test_programs.py
@@ -1,10 +1,73 @@
 """Test some LL programs to see if the output is correct"""
 import json
-import pytest  # noqa
-from . import prog_test
-
+import pytest
+from . import prog_test, run_prog
+# flake8: noqa
+# Have to ignore the linter here because of the verbose string.
+# Maybe could move expected output out to a file.
 
 @pytest.mark.parametrize('prog', json.load(open("tests/programs.json")))
 def test_programs(prog):
     """Iterate over and test programs"""
     prog_test(prog)
+
+
+def test_verbose():
+    with open('tests/programs.json') as f:
+        data = json.load(f)
+    helloworld = data[0]
+    stdout = run_prog(helloworld['path'], '', verbose=True)
+    assert stdout == r"""\   /\ 
+\"Hel/ 
+/dlrow\
+"   \ /
+#      
+       
+addr: 0 - stack: []
+\
+addr: 0 - stack: []
+\
+addr: 0 - stack: []
+"
+addr: 0 - stack: []
+H
+addr: 0 - stack: []
+e
+addr: 0 - stack: []
+l
+addr: 0 - stack: []
+/
+addr: 0 - stack: []
+\
+addr: 0 - stack: []
+/
+addr: 0 - stack: []
+l
+addr: 0 - stack: []
+o
+addr: 0 - stack: []
+\
+addr: 0 - stack: []
+ 
+addr: 0 - stack: []
+/
+addr: 0 - stack: []
+\
+addr: 0 - stack: []
+w
+addr: 0 - stack: []
+o
+addr: 0 - stack: []
+r
+addr: 0 - stack: []
+l
+addr: 0 - stack: []
+d
+addr: 0 - stack: []
+/
+addr: 0 - stack: []
+"
+addr: 0 - stack: ['Hello world']
+#
+Hello world 
+"""
