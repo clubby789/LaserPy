b7ee72fcf2dbe6b17d42e247044cc0436eb27469
clubby789 <jamie@hill-daniel.co.uk>
2020-10-18 16:55:38 +0100

    Add testing of errors

---
+++ tests/test_errors.py
@@ -0,0 +1,12 @@
+"""Test some LL programs to see if the output is correct"""
+import json
+import pytest
+from . import prog_test
+import subprocess
+
+
+@pytest.mark.parametrize('prog', json.load(open("tests/errors.json")))
+def test_errors(prog):
+    """Iterate over and test programs that will cause errors"""
+    with pytest.raises(subprocess.CalledProcessError):
+        prog_test(prog)
