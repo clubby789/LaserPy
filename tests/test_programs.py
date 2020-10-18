74aca66617860e86dc02246c4103a895a6c590fe
clubby789 <jamie@hill-daniel.co.uk>
2020-10-18 12:09:50 +0100

    Pylint the tests

--- tests/test_programs.py
+++ tests/test_programs.py
@@ -1,19 +1,23 @@
+"""Test some LL programs to see if the output is correct"""
 import subprocess
 import json
 import pytest  # noqa
 
 
 def prog_test(program):
+    """Test a single program"""
     if program['args'] is None:
         cmd = f"python3 laser.py {program['path']}"
     else:
         cmd = f"python3 laser.py {program['path']} {program['args']}"
     print(cmd)
     stdout = subprocess.run(cmd.split(),
-                            stdout=subprocess.PIPE).stdout.decode()
+                            stdout=subprocess.PIPE,
+                            check=True).stdout.decode()
     assert stdout == program["expected"]
 
 
 @pytest.mark.parametrize('prog', json.load(open("tests/tests.json")))
 def test_programs(prog):
+    """Iterate over and test programs"""
     prog_test(prog)
